3e3c7e6d15c1c97c18041bbc38026fd5847fa8bf
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2022-10-12 10:45:38 -0230

    animations

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-const observer = new IntersectionObserver((entries) => {
+const observerX = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     console.log(entry);
     if (entry.isIntersecting) {
-      entry.target.classList.add("show");
+      entry.target.classList.add("showX");
     } else {
-      entry.target.classList.remove("show");
+      entry.target.classList.remove("showX");
     }
   });
 });
 
-const hiddenElements = document.querySelectorAll(".hidden");
-hiddenElements.forEach((el) => observer.observe(el));
+const hiddenElementsX = document.querySelectorAll(".hiddenX");
+hiddenElementsX.forEach((el) => observerX.observe(el));
 
 const hiddenSplashElements = document.querySelectorAll(".hiddenSplash");
-hiddenSplashElements.forEach((el) => observer.observe(el));
+hiddenSplashElements.forEach((el) => observerX.observe(el));
+
+const observerY = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("showY");
+    } else {
+      entry.target.classList.remove("showY");
+    }
+  });
+});
+
+const hiddenElementsY = document.querySelectorAll(".hiddenY");
+hiddenElementsY.forEach((el) => observerY.observe(el));
